466214110caffcf6666d7bdb67b7230c04a57155
a.strunin <a.strunin13@gmail.com>
2018-07-05 14:55:23 +0300

    Edit description in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='mkreport-create',
     version='0.01',
     packages=find_packages(),
-    long_description="This script can be used to generate reports from makefile output and PVS Studio csv reports",
+    long_description="The Python3 script can be used to make reports from makefile output and PVS Studio csv reports",
 
 
     scripts=[
@@ -17,3 +17,4 @@ setup(
         'XlsxWriter',
     ]
 )
+
